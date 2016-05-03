ac078043791e5de0a8860c9bbce180dfe1231895
Carlos Velasco <carlos@2activedesign.com>
2016-05-03 14:46:28 +0200

    Add css file and remove comments

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 abc = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
 
-construct = {name: 'abc', class: ''};
-letters =  [];
+construct = {
+    name: 'abc',
+    class: ''
+};
+letters = [];
 
-for (i = 0; i < abc.length; i++){
-    //console.log(i);
-    //console.log(abc[i]);
+for (i = 0; i < abc.length; i++) {
     letters[i] = Object.create(construct);
     letters[i].name = abc[i];
-    //console.log(letters[i]);
 };
 
-pick = function(id){
+pick = function(id) {
     letters[id].class = "picked";
-    //console.log(id);
     el = document.getElementById(id);
-    //console.log(el);
     el.classList.add("picked");
     el.disabled = true;
     return id;
 };
 
-listAvailable = function(){
-    for (i = 0; i < abc.length; i++){
-    if (letters[i].class == 'unpicked'){
-        console.log(letters[i].name + ' ' + letters[i].class);
-    } else {
-        console.log(letters[i].name + ' ' + letters[i].class);
+listAvailable = function() {
+    for (i = 0; i < abc.length; i++) {
+        if (letters[i].class == 'unpicked') {
+            console.log(letters[i].name + ' ' + letters[i].class);
+        } else {
+            console.log(letters[i].name + ' ' + letters[i].class);
+        }
     }
-}
 };
 
-populate = function(reset){
+populate = function(reset) {
     var data = "";
-    for (i = 0; i < abc.length; i++){
-        if (reset) {letters[i].class = "";}
+    for (i = 0; i < abc.length; i++) {
+        if (reset) {
+            letters[i].class = "";
+        }
         name = letters[i].name;
         classes = letters[i].class;
-        clickEvent = "pick('button#"+i +", '"+i+"'');";
-    data = data + "<button id='"+i+"' class='frame "+classes+"' onclick="+'pick('+i+');'+">"+name+"</button>";
+        clickEvent = "pick('button#" + i + ", '" + i + "'');";
+        data = data + "<button id='" + i + "' class='frame " + classes + "' onclick=" + 'pick(' + i + ');' + ">" + name + "</button>";
     }
 
     document.getElementById('abcs').innerHTML = data;
