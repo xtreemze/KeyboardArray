480abc63c3f0c454b00f572ff5d5b2879c92dd97
Carlos Velasco <carlos@2activedesign.com>
2016-05-16 16:47:39 +0200

    updates to standardize

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+// !minifyOnSave
+// minifyOnSave: false
+// !minOnSave
+// minOnSave: false
+module.exports = {
+  "ecmaVersion": 5,
+  "ecmaFeatures": {
+    "jsx": false,
+    "modules": false,
+    "arrowFunctions": false,
+    "classes": false,
+    "spread": false,
+    "impliedStrict": true
+  },
+  "env": {
+    "browser": true,
+    "jquery": true,
+  },
+  "extends": "airbnb",
+  "installedESLint": true,
+  "plugins": ["react"],
+  "rules": {
+    // "indent": [ "error", "tab" ],
+    // "linebreak-style": [ "error", "windows" ],
+    // "quotes": [ "error", "single" ],
+    // "semi": [ "error", "always" ]
+    // "newline-per-chained-call": ["off"],
+    // "no-var": ["off"],
+    // "max-len": ["off", 100, 4],
+    // "radix": ["off"],
+    // "prefer-template": ["off"],
+    // "prefer-arrow-callback": ["off"],
+    // "func-names": ["off"],
+    // "no-use-before-define": ["off"]
+  },
+  "globals": {
+    "Materialize": false
+  }
+};
